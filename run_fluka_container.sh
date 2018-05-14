6fdb4981c067ae688218bda9fe16784e6884d022
afontana68 <35598608+afontana68@users.noreply.github.com>
2018-05-14 19:12:36 +0200

    Update run_fluka_container.sh

--- run_fluka_container.sh
+++ run_fluka_container.sh
@@ -1,1 +1,1 @@
-docker run -i --rm --name fluka --net=host -e DISPLAY=192.168.0.100:0.0 -v ./docker_work:/docker_work -t my_fedora_27-fluka bash
+docker run -i --rm --name fluka --net=host -e DISPLAY=192.168.0.100:0.0 -v $PWD/docker_work:/docker_work -t my_fedora_27-fluka bash
