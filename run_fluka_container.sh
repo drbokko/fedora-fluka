23078e8e7cceef5552b14d431d9b52f0d8348a35
Andrea Fontana <andrea.fontana@pv.infn.it>
2018-05-15 17:20:23 +0200

    Updated run script for linux

--- run_fluka_container.sh
+++ run_fluka_container.sh
@@ -4,4 +4,5 @@
 # dr.vittorio.boccone@ieee.org
 # vittorio.boccone@dectris.com
 # andrea.fontana@pv.infn.it
-docker run -i --rm --name fluka --net=host -e DISPLAY=192.168.0.100:0.0 -v $PWD/docker_work:/docker_work -t my_fedora_27-fluka bash
+docker run -i --rm --name fluka --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $PWD/docker_work:/docker_work -t my_fedora_27-fluka bash
+e
