be542e11ef7c0b6aa3bebc88f9c93b5c825cd737
afontana68 <35598608+afontana68@users.noreply.github.com>
2018-05-15 11:52:28 +0200

    Update run_fluka_container.sh

--- run_fluka_container.sh
+++ run_fluka_container.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# Script to run a Customized Docked Fedora 27 for running Fluka 
+# ========================================================
+# dr.vittorio.boccone@ieee.org
+# vittorio.boccone@dectris.com
+# andrea.fontana@pv.infn.it
 docker run -i --rm --name fluka --net=host -e DISPLAY=192.168.0.100:0.0 -v $PWD/docker_work:/docker_work -t my_fedora_27-fluka bash
