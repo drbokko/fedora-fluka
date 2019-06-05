942e3a0d1101405ed4573796c5e3cd2d8e6c7b07
Vittorio Boccone <vittorio.boccone@dectris.com>
2019-06-05 17:13:50 +0200

    adding user to the flukauser group

--- .ci_docker_hub/docker-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Setting up home folder pass through"
-set -xe
-
-groupadd -g ${GROUP_ID} ${USER_NAME}
-useradd -u ${USER_ID} -g ${USER_NAME}  ${USER_NAME} --no-create-home
-echo "${USER_NAME} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-su ${USER_NAME}
