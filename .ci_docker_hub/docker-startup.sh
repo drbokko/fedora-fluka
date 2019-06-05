f070bd25732f8802c8e61b3c115be08bd3e6a956
Vittorio Boccone <vittorio.boccone@dectris.com>
2019-06-05 17:11:16 +0200

    adding user to the flukauser group

--- .ci_docker_hub/docker-startup.sh
+++ .ci_docker_hub/docker-startup.sh
@@ -5,6 +5,5 @@ set -xe
 groupadd -g ${GROUP_ID} ${USER_NAME}
 useradd -u ${USER_ID} -g ${USER_NAME}  ${USER_NAME} --no-create-home
 echo "${USER_NAME} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-usermod -a -G fluka ${USER_NAME}
 
 su ${USER_NAME}
