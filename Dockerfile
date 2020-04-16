071b9a805232806b08ec6c6221a7277448f882ff
Vittorio Boccone <vittorio.boccone@dectris.com>
2020-04-16 11:21:27 +0200

    adding new fluka

--- Dockerfile
+++ Dockerfile
@@ -8,24 +8,26 @@ ARG fedora_for_fluka_repo
 
 FROM $fedora_for_fluka_repo
 ARG fluka_package
-
+RUN dnf install -y gfortran gcc-gfortran-9.2.1
 
 # Copy fluka to local folder
 COPY $fluka_package /tmp
 
-RUN tar -zxvf /tmp/*.tar.gz -C /opt/fluka
-ENV FLUFOR=gfortran
-ENV FLUPRO=/opt/fluka
-RUN cd /opt/fluka; make
+RUN dnf install -y /tmp/$fluka_package
+# RUN mkdir -p /opt/fluka
+# RUN tar -zxvf /tmp/*.tar.gz -C /opt/fluka
+# ENV FLUFOR=gfortran
+# ENV FLUPRO=/opt/fluka
+# RUN cd /opt/fluka; make
 
-RUN chown -R fluka:fluka /opt/fluka
-RUN chmod -R g+rw /opt/fluka
-RUN chmod g+x /opt/fluka/flutil/fff
-RUN chmod g+x /opt/fluka/flutil/rfluka
-RUN chmod g+x /opt/fluka/flutil/ldpmqmd
-RUN chmod g+x /opt/fluka/flutil/lfluka
-RUN chmod g+x /opt/fluka/flutil/lflukac
-RUN chmod g+x /opt/fluka/flutil/pawlevphi.kumac
+# RUN chown -R fluka:fluka /opt/fluka
+# RUN chmod -R g+rw /opt/fluka
+# RUN chmod g+x /opt/fluka/flutil/fff
+# RUN chmod g+x /opt/fluka/flutil/rfluka
+# RUN chmod g+x /opt/fluka/flutil/ldpmqmd
+# RUN chmod g+x /opt/fluka/flutil/lfluka
+# RUN chmod g+x /opt/fluka/flutil/lflukac
+# RUN chmod g+x /opt/fluka/flutil/pawlevphi.kumac
 
 # Remove tmp file
 RUN rm -rf /tmp/*.gz
--- build_fluka_container.sh
+++ build_fluka_container.sh
@@ -4,10 +4,11 @@
 # dr.vittorio.boccone@ieee.org
 # vittorio.boccone@dectris.com
 
-fluka_version="2011.2x"
-fluka_respin="6"
+fluka_version="2011-3.0"
+fluka_respin="Linux-gfor9"
 
-fluka_tarball="fluka${fluka_version}-linux-gfor64bitAA.tar.gz"
+fluka_tarball="fluka-${fluka_version}.${fluka_respin}.x86_64.rpm"
+# fluka-2011-3.0.Linux-gfor9.x86_64.rpm
 
 fluka_package=$fluka_tarball
 USER_NAME=$(whoami)
@@ -23,18 +24,9 @@ else
   fi
 fi
 
-if [ ! -e ${fluka_package} ]; then 
-   echo "Downloading Fluka"
-   echo "Please specify your Fluka user identification ('fuid', i.e. fuid-1234)"
-   echo -n "fuid: "
-   read fuid
-
-   wget --user=$fuid --ask-password  https://www.fluka.org/packages/${fluka_package}
-fi
-
 if [ ! -e ${fluka_package} ]; then
   echo "Error downloading Fluka package [${fluka_package}]"
   exit 1
 fi
 
-docker build --build-arg fedora_for_fluka_repo=$fedora_for_fluka_repo --build-arg fluka_package=$fluka_package -t fedora_with_fluka_for_${USER_NAME}:$fluka_version.$fluka_respin -t fedora_with_fluka_for_${USER_NAME} .
+docker build --no-cache --build-arg fedora_for_fluka_repo=$fedora_for_fluka_repo --build-arg fluka_package=$fluka_package -t fedora_with_fluka_for_${USER_NAME}:$fluka_version.$fluka_respin -t fedora_with_fluka_for_${USER_NAME} .
